79b9d522b1eec90fbfd37301bd12f478559489c4
Viacheslav Severyn <sever_2008@ukr.net>
2022-08-05 00:41:35 +0300

    logics done

---
+++ contacts.js
@@ -0,0 +1,59 @@
+const fs = require("fs/promises");
+const path = require("path");
+const { customAlphabet } = require("nanoid");
+
+const nanoid = customAlphabet("1234567890", 2);
+
+const contactsPath = path.join(__dirname, "/db/contacts.json");
+
+async function updatingContacts(data) {
+  await fs.writeFile(contactsPath, JSON.stringify(data, null, 2));
+}
+
+async function listContacts() {
+  const contacts = await fs.readFile(contactsPath);
+
+  return JSON.parse(contacts);
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const contact = contacts.find((contact) => contact.id === contactId);
+
+  return contact || null;
+}
+
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  await updatingContacts(contacts);
+
+  return newContact;
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const idx = contacts.findIndex((contact) => contact.id === contactId);
+
+  if (idx === -1) {
+    return null;
+  }
+
+  const removedContact = contacts.splice(idx, 1);
+  await updatingContacts(contacts);
+
+  return removedContact;
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
---
+++ index.js
@@ -0,0 +1,33 @@
+const API = require("./contacts");
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const contacts = await API.listContacts();
+      console.log(contacts);
+      break;
+
+    case "get":
+      const contact = await API.getContactById(id);
+      console.log(contact);
+      break;
+
+    case "add":
+      const newContact = await API.addContact(name, email, phone);
+      console.log(newContact);
+      break;
+
+    case "remove":
+      const removedContact = await API.removeContact(id);
+      console.log(removedContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction({
+  action: "get",
+  id: "9",
+});
